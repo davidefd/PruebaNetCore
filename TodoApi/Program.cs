dc2491735f5f290275638bf28a265f4158657639
David Figueroa <davidefd0001@gmail.com>
2024-03-01 22:00:10 -0600

    Añadiendo código a Program.cs para la conexión a la base de datos

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -1,4 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Context;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//Creando variable para cadena de conexión a la base de datos
+var connectionString = builder.Configuration.GetConnectionString("Connection");
+//Registrando servicio para la conexión
+builder.Services.AddDbContext<AppDbContext> (options => options.UseSqlServer(connectionString));
 
 // Add services to the container.
 
