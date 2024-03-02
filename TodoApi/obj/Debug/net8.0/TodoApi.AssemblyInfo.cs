42fec6ef69f5b0e4b2664ba2ba82179d4ac3f3b4
David Figueroa <davidefd0001@gmail.com>
2024-03-01 22:24:12 -0600

    Guardando cambios después de probar api

--- TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5040cfa2817aa757bb386a5099d7e7c4f68e5e2d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75eb6ff8a11a60c10505805363ffc7339c616386")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
